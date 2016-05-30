9af3f38b242c16eae1081727c93cfd022f04e4b4
Chris Maloney <maloneyc@ncbi.nlm.nih.gov>
2016-05-30 19:59:02 -0400

    use config1 with the treechart demo

--- src/demo.js
+++ src/demo.js
@@ -1,79 +1,75 @@
-// Fetch the word list
-
-fetch('node_modules/word-list-json/words.json')
-.then(response => {
-  if (response.status !== 200) {
-    console.error('Problem fetching word list');
-    return;
-  }
-  return response.json();
-})
-.then(wordsJson => {
-  TreeChart.WordList = wordsJson.words;
-  treeDemo();
-})
-.catch(err => {
-  console.error('Error while fetching word list: ', err);
-});
-
 
 
 // The main loop is controlled by a timer that emits poisson process events,
 // random events with an average frequency of `speed` Hz.
 // The timer is always going; when the looping is disabled, we just ignore it.
 
-class Looper {
-  constructor() {
-    this.enabled = true;
-    this.speed = 0.5;
-    this.nextEvent = () => -1000 * Math.log(Math.random()) / this.speed;
-    this.listen();
-    this.ticker();
-  }
+const Demo = (function() {
+  'use strict';
 
-  get ticker() {
-    return () => {
-      if (this.enabled) this.tick();
-      setTimeout(this.ticker, this.nextEvent());
-    };
-  }
+  // const C1 = require('config-one');
+  const C1 = config1;
 
-  tick() {
-    console.log(TreeChart.Words.pick());
-  }
+  // In the case of this class, the options define the initial state of an
+  // instance. All of the functions here get bound to the instance when it
+  // is constructed. 
 
-  disable() {
-    this.enabled = false;
-  }
-
-  enable() {
-    this.enabled = true;
-  }
-
-  faster() {
-    this.speed *= 1.1;
-  }
-
-  slower() {
-    this.speed /= 1.1;
-  }
-
-  listen() {
-    document.onkeydown = evt => {
-      if (evt.keyCode == 27) this.disable();  // esc
-      if (evt.keyCode == 32) this.enable();   // space
-      if (evt.keyCode == 189) this.slower();  // -
-      if (evt.keyCode == 187) this.faster();  // = or + key
-      console.log(evt);
-    };
+  const defaults = {
+    enabled: true,
+    speed: 1,
+    nextDelay: function() { 
+      return -1000 * Math.log(Math.random()) / this.speed; 
+    },
+    listeners: [
+      { type: 'keydown',
+        filter: evt => evt.keyCode == 27,  // escape
+        listener: function() { 
+          console.log('escape: enabled = ' + this.enabled);
+          this.enabled = false; },
+      },
+      { type: 'keydown',
+        filter: evt => evt.keyCode == 32,  // space
+        listener: function() { this.enabled = true; },
+      },
+      { type: 'keydown',
+        filter: evt => evt.keyCode == 189,  // `-`
+        listener: function() { this.speed *= 1.1; },
+      },
+      { type: 'keydown',
+        filter: evt => evt.keyCode == 187,  // `=` or `+`
+        listener: function() { this.speed /= 1.1; },
+      },
+    ],
   };
-};
 
+  return class {
+    constructor(_opts = {}) {
+      this.options = C1.extend(defaults, _opts);
+      // Assign the options to this instance itself
+      Object.assign(this, this.options);
 
-// Main entry point
+      // Set event listeners
+      const self = this;
+      this.listeners.forEach(spec => {
+        const dispatch = evt => 
+          spec.filter(evt) && spec.listener.call(self, evt);
+        document.addEventListener(spec.type, dispatch);
+      });
+    }
 
-function treeDemo() {
-  console.log('Number of words: ' + TreeChart.Words.length);
-  const looper = new Looper();
-}
+    get ticker() {
+      return () => {
+        if (this.enabled) this.tick();
+        setTimeout(this.ticker, this.nextDelay());
+      };
+    }
 
+    start() {
+      this.ticker();
+    }
+
+    tick() {
+      console.log('tick');
+    }
+  };
+})();
--- work.md
+++ work.md
@@ -3,6 +3,9 @@ To do:
 # Queue
 
 * Strategy:
+    * No words in the basic library. But keep that stuff around, split into
+      separate files.
+    * The demo is based on config1 too (natch) and extensible
     * Then TDD: start the demo index.html page, and develop as needed to make
       it work.
 
